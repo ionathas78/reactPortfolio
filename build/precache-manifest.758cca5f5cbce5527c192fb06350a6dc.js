aace33ed8ec721a9ca899f2e5c836f7d75ff49e1
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-29 09:37:58 -0500

    take 2

--- build/precache-manifest.758cca5f5cbce5527c192fb06350a6dc.js
+++ build/precache-manifest.758cca5f5cbce5527c192fb06350a6dc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bfbe86b5c4f391cb9b00d316eab4d79",
+    "revision": "733ffe368908377294e3151241b7fcd3",
     "url": "/index.html"
   },
   {
-    "revision": "85f4359d7555aeab2d62",
+    "revision": "d1e4961726c828d1978b",
     "url": "/static/css/main.a87aa558.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a9f37d7b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85f4359d7555aeab2d62",
-    "url": "/static/js/main.b1b89851.chunk.js"
+    "revision": "d1e4961726c828d1978b",
+    "url": "/static/js/main.1d5c6d8c.chunk.js"
   },
   {
     "revision": "b4ba650b238755144e2a",
