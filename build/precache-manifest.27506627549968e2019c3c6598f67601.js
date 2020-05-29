f886c7bb17a78eceb8e70cf878c7164480f7bffe
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-29 09:36:51 -0500

    gh-pages fix

---
+++ build/precache-manifest.27506627549968e2019c3c6598f67601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfbe86b5c4f391cb9b00d316eab4d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f4359d7555aeab2d62",
+    "url": "/static/css/main.a87aa558.chunk.css"
+  },
+  {
+    "revision": "78846b30868491734423",
+    "url": "/static/js/2.a9f37d7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9f37d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f4359d7555aeab2d62",
+    "url": "/static/js/main.b1b89851.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
