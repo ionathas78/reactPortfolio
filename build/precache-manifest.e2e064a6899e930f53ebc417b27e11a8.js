bbf52a7b340ae74ae73c36e920e8a039a6d5acb3
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-31 01:05:25 -0500

    deployed

---
+++ build/precache-manifest.e2e064a6899e930f53ebc417b27e11a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882828c8790627218407b1065fe5a75b",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "39f8a915f4a4d50fca45",
+    "url": "/reactPortfolio/static/css/main.7caeacdd.chunk.css"
+  },
+  {
+    "revision": "d9cf7d7be874a2c7dc7d",
+    "url": "/reactPortfolio/static/js/2.befed2ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.befed2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f8a915f4a4d50fca45",
+    "url": "/reactPortfolio/static/js/main.ad2d5b27.chunk.js"
+  },
+  {
+    "revision": "a26e196d607cff38a192",
+    "url": "/reactPortfolio/static/js/runtime-main.aca8b71c.js"
+  }
+]);
